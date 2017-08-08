55e37a87001186435ffd6e75128260e0c4281a05
kh355680 <kh355680@hotmail.com>
2017-08-08 11:19:38 -0700

    category html added

--- app/app.js
+++ app/app.js
@@ -1,9 +1,8 @@
 (function() {
   "use strict";
 
-  angular
-    .module("app", ["ui.router"])
-    .config(function($stateProvider, $urlRouterProvider) {
+  angular.module("app", ["ui.router"])
+  .config(function($stateProvider, $urlRouterProvider) {
         $stateProvider.state('home',{
             url:'/home',
             templateUrl:'app/home/home.html'
@@ -11,10 +10,13 @@
         $stateProvider.state('categoryList',{
             url:'/category-list',
             templateUrl:'app/category/category-list.html'
-        });
+        })
+        $stateProvider.state('newCategory',{
+            url:'/new-category',
+            templateUrl:'app/category/new-category.html'
+        });        
         $urlRouterProvider.when('','/home');
     })
     .run(function() {
-      console.log("loaded");
     });
 })();
